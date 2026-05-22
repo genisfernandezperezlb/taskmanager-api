db5e0ee072d5e58473b914ef47b4d659fa631165
Genis Fernandez <genisfernandezperezlb@gmail.com>
2026-05-22 12:17:59 +0200

    Initial backend setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
+	runtimeOnly("org.postgresql:postgresql:42.7.3")
 }
 
 kotlin {
